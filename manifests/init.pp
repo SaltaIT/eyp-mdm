7da569836d9e21d67f941601e567880847569950
Jordi Prats <jordi.prats@gmail.com>
2019-04-17 11:31:22 +0200

    wip

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,23 @@
 class mdm (
+            $password_minlen                = '14',
+            $password_complexity_digits_credit               = '-1',
+            $password_complexity_uppercase_credit               = '-1',
+            $password_complexity_other_credit               = '-1',
+            $password_complexity_lowercase_credit               = '-1',
+            $remember_old_passwords         = '12',
           ) inherits mdm::params{
+
+  include ::system
+
+  class { 'pam::cracklib':
+    minlen  => $password_minlen,
+    dcredit => $password_complexity_digits_credit,
+    ucredit => $password_complexity_uppercase_credit,
+    ocredit => $password_complexity_other_credit,
+    lcredit => $password_complexity_lowercase_credit,
+  }
+
+  class { 'pam::unix':
+    remember => $remember_old_passwords,
+  }
 }
